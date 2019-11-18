509d38983f73210d59e2184bf1f7b51e0d6f2a17
shubhamdas <shubhamid@gmail.com>
2019-11-18 06:02:44 -0500

    backend ready

---
+++ concertfit/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using concertfit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace concertfit.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public List<ConcertfitResponse> Get()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string jsonDataTicketmaster = webClient.DownloadString("https://concertfitapis20191118021024.azurewebsites.net/api/ticketmaster");
+                TicketMasterResponse.TopLevel topLevelTicketMaster = TicketMasterResponse.TopLevel.FromJson(jsonDataTicketmaster);
+
+                string jsonDataSpotify = webClient.DownloadString("https://concertfitapis20191118021024.azurewebsites.net/api/spotify");
+                SpotifyArtistsResponse.TopLevel[] topLevelSpotify = SpotifyArtistsResponse.TopLevel.FromJson(jsonDataSpotify);
+
+                List<ConcertfitResponse> concertfitResponseList = new List<ConcertfitResponse>();
+
+                IDictionary<string, TicketMasterResponse.Event> events = new Dictionary<string, TicketMasterResponse.Event>();
+                foreach (TicketMasterResponse.Event evnt in topLevelTicketMaster.Embedded.Events)
+                {
+                    events.TryAdd(evnt.Name, evnt);
+                }
+
+                foreach (SpotifyArtistsResponse.TopLevel topLevel in topLevelSpotify)
+                {
+                    ConcertfitResponse concertfitResponse = new ConcertfitResponse();
+                    concertfitResponse.Artist = topLevel.Artist;
+                    concertfitResponse.Track = topLevel.Tracks;
+                    concertfitResponse.EventName = events[topLevel.Artist].Name;
+                    concertfitResponse.EventUrl = events[topLevel.Artist].Url.ToString();
+                    concertfitResponse.Dates = events[topLevel.Artist].Dates;
+                    concertfitResponseList.Add(concertfitResponse);
+                }
+
+                return concertfitResponseList;
+            }
+        }
+    }
+}
---
+++ concertfit/Models/ConcertfitResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace concertfit.Models
+{
+    public class ConcertfitResponse
+    {
+        [JsonProperty("artist")]
+        public string Artist { get; set; }
+
+        [JsonProperty("track")]
+        public SpotifyArtistsResponse.Track[] Track { get; set; }
+
+        [JsonProperty("eventName")]
+        public string EventName { get; set; }
+
+        [JsonProperty("eventUrl")]
+        public string EventUrl { get; set; }
+
+        [JsonProperty("dates")]
+        public TicketMasterResponse.Dates Dates { get; set; }
+    }
+}
