5d78b68fb6f3f213c1ecf9d40fed9fb5f17d8ea6
parthbhimani <54642042+parthbhimani@users.noreply.github.com>
2019-11-18 19:06:41 -0500

    CodeReviewAssignment - bhimanpm

--- concertfit/Controllers/HomeController.cs
+++ concertfit/Controllers/HomeController.cs
@@ -15,36 +15,41 @@ namespace concertfit.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        public List<ConcertfitResponse> Get()
+        public JsonResult Get()
+        {
+            string jsonDataTicketmaster = GetJsonString("https://concertfitapis20191118021024.azurewebsites.net/api/ticketmaster");
+            TicketMasterResponse.TopLevel topLevelTicketMaster = TicketMasterResponse.TopLevel.FromJson(jsonDataTicketmaster);
+
+            string jsonDataSpotify = GetJsonString("https://concertfitapis20191118021024.azurewebsites.net/api/spotify");
+            SpotifyArtistsResponse.TopLevel[] topLevelSpotify = SpotifyArtistsResponse.TopLevel.FromJson(jsonDataSpotify);
+
+            List<ConcertfitResponse> concertfitResponseList = new List<ConcertfitResponse>();
+
+            IDictionary<string, TicketMasterResponse.Event> events = new Dictionary<string, TicketMasterResponse.Event>();
+            foreach (TicketMasterResponse.Event evnt in topLevelTicketMaster.Embedded.Events)
+            {
+                events.TryAdd(evnt.Name, evnt);
+            }
+
+            foreach (SpotifyArtistsResponse.TopLevel topLevel in topLevelSpotify)
+            {
+                ConcertfitResponse concertfitResponse = new ConcertfitResponse();
+                concertfitResponse.Artist = topLevel.Artist;
+                concertfitResponse.Track = topLevel.Tracks;
+                concertfitResponse.EventName = events[topLevel.Artist].Name;
+                concertfitResponse.EventUrl = events[topLevel.Artist].Url.ToString();
+                concertfitResponse.Dates = events[topLevel.Artist].Dates;
+                concertfitResponseList.Add(concertfitResponse);
+            }
+
+            return new JsonResult(concertfitResponseList);
+        }
+
+        private string GetJsonString(string url)
         {
             using (WebClient webClient = new WebClient())
             {
-                string jsonDataTicketmaster = webClient.DownloadString("https://concertfitapis20191118021024.azurewebsites.net/api/ticketmaster");
-                TicketMasterResponse.TopLevel topLevelTicketMaster = TicketMasterResponse.TopLevel.FromJson(jsonDataTicketmaster);
-
-                string jsonDataSpotify = webClient.DownloadString("https://concertfitapis20191118021024.azurewebsites.net/api/spotify");
-                SpotifyArtistsResponse.TopLevel[] topLevelSpotify = SpotifyArtistsResponse.TopLevel.FromJson(jsonDataSpotify);
-
-                List<ConcertfitResponse> concertfitResponseList = new List<ConcertfitResponse>();
-
-                IDictionary<string, TicketMasterResponse.Event> events = new Dictionary<string, TicketMasterResponse.Event>();
-                foreach (TicketMasterResponse.Event evnt in topLevelTicketMaster.Embedded.Events)
-                {
-                    events.TryAdd(evnt.Name, evnt);
-                }
-
-                foreach (SpotifyArtistsResponse.TopLevel topLevel in topLevelSpotify)
-                {
-                    ConcertfitResponse concertfitResponse = new ConcertfitResponse();
-                    concertfitResponse.Artist = topLevel.Artist;
-                    concertfitResponse.Track = topLevel.Tracks;
-                    concertfitResponse.EventName = events[topLevel.Artist].Name;
-                    concertfitResponse.EventUrl = events[topLevel.Artist].Url.ToString();
-                    concertfitResponse.Dates = events[topLevel.Artist].Dates;
-                    concertfitResponseList.Add(concertfitResponse);
-                }
-
-                return concertfitResponseList;
+                return webClient.DownloadString(url);
             }
         }
     }
