ea6052f3c4d24f649adb2af72fe0f54802a39e47
Ajinkya Dalvi <dalviap@mail.uc.edu>
2019-11-20 10:22:07 -0500

    Ajinkya_XML_Code Review

--- concertfit/Pages/Index.cshtml.cs
+++ concertfit/Pages/Index.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using concertfit.Controllers;
+using System.Net;
+using SpotifyArtistsResponse;
+using concertfit.Models;
 namespace concertfit.Pages
 {
     public class IndexModel : PageModel
     {
         public void OnGet()
         {
+            using (WebClient webClient = new WebClient())
+            {
 
+                string jsonDataSpotify = webClient.DownloadString("https://concertfitapis20191118021024.azurewebsites.net/api/spotify");
+                var topLevel = TopLevel.FromJson(jsonDataSpotify);
+                ViewData["SpotifyMaster"] = topLevel;
+
+
+            }
         }
     }
 }
